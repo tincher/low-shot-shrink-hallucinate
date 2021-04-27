92cc1745984e7b35c97d0ff4e2029283cbf0c3bd
joel <joel.ewig@stud.uni-bamberg.de>
2021-04-27 18:11:32 +0200

    debug

--- ResNetBasic.py
+++ ResNetBasic.py
@@ -153,6 +153,7 @@ class ResNet(nn.Module):
         self.trunk = nn.Sequential(*trunk)
         self.final_feat_dim = indim
         if not only_trunk:
+            print(indim, num_classes)
             self.classifier = nn.Linear(indim, num_classes)
             self.classifier.bias.data.fill_(0)
 
