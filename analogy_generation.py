e7c3da524e3d6e5ef0acfaea07f6ecde4455977d
joel <joel.ewig@stud.uni-bamberg.de>
2021-04-30 10:51:55 +0200

    debug

--- analogy_generation.py
+++ analogy_generation.py
@@ -150,6 +150,8 @@ def train_analogy_regressor(analogies, centroids, base_classes, trained_classifi
 
     num_clusters_per_class = centroids[0].shape[0]
     centroid_labels = (np.array(base_classes).reshape((-1,1)) * np.ones((1, num_clusters_per_class))).reshape(-1)
+    print(base_classes)
+
     concatenated_centroids = np.concatenate(centroids, axis=0)
 
 
