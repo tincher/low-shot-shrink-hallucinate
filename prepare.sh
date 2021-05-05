fe6c4ff663906c3686e2b258ea060ddc0f9b4b35
joel <joel.ewig@stud.uni-bamberg.de>
2021-05-05 16:01:32 +0200

    debug

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # save features
+dir="/home/user59/bachelor/neural_nets/augmentation/low-shot-shrink-hallucinate"
 mkdir -p features/ResNet10_sgm
-python3 ./save_features.py \
+python3 $dir/save_features.py \
   --cfg train_save_data.yaml \
   --outfile features/ResNet10_sgm/train.hdf5 \
   --modelfile checkpoints/ResNet10_sgm/89.tar \
   --model ResNet10 \
   --num_classes 56
 
-python3 ./save_features.py \
+python3 $dir/save_features.py \
   --cfg val_save_data.yaml \
   --outfile features/ResNet10_sgm/val.hdf5 \
   --modelfile checkpoints/ResNet10_sgm/89.tar \
